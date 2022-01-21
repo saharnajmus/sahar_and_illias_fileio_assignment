209daf7f679204aa88ddb55a01e39b37e51b98d7
sahar <saharnajmus91@gmail.com>
2022-01-21 15:22:38 +0100

    Correct Path is added

--- src/be/intecbrussel/FileIOApp.java
+++ src/be/intecbrussel/FileIOApp.java
@@ -1,6 +1,5 @@
 package be.intecbrussel;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 
@@ -9,7 +8,7 @@ public class FileIOApp {
 
         try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get("C:/Users/uzair/Downloads/unsorted/unsorted"))) {
             for (Path file : stream) {
-                createDirectory(file);
+                createDirectoryForTextFiles(file);
                 // System.out.println("folder is created");
                 // Path path = Paths.get("C:/Users");
 
@@ -21,24 +20,78 @@ public class FileIOApp {
         }
     }
 
-    private static void createDirectory(Path path) {
+    private static void createDirectoryForTextFiles(Path path) {
         if (path.toString().endsWith(".txt")) {
-            // String stringPath = path.toString();
-            //  int indexOfTxt = stringPath.indexOf("txt");
-            try {
-                Path directoryPath = Paths.get("C:/assignment/file_io/sorted");
-               // if (Files.notExists(directoryPath)) {
-                    Files.createDirectories(directoryPath);
-               // }
+            Path directoryPath = Paths.get("C:/assignment/file_io/sorted/txt");
+            createDirectory(path,directoryPath);
+        }
+        else if(path.toString().endsWith(".png")){
+            Path directoryPath = Paths.get("C:/assignment/file_io/sorted/png");
+            createDirectory(path,directoryPath);
+        }
+        else if(path.toString().endsWith(".gif")){
+            Path directoryPath = Paths.get("C:/assignment/file_io/sorted/gif");
+            createDirectory(path,directoryPath);
+        }
+        else if(path.toString().endsWith(".jpg")){
+            Path directoryPath = Paths.get("C:/assignment/file_io/sorted/jpg");
+            createDirectory(path,directoryPath);
+        }
+        else if(path.toString().endsWith(".csv")){
+            Path directoryPath = Paths.get("C:/assignment/file_io/sorted/csv");
+            createDirectory(path,directoryPath);
+        }
+        else if(path.toString().endsWith(".exe")){
+            Path directoryPath = Paths.get("C:/assignment/file_io/sorted/exe");
+            createDirectory(path,directoryPath);
+        }
+        else if(path.toString().endsWith(".pdf")){
+            Path directoryPath = Paths.get("C:/assignment/file_io/sorted/pdf");
+            createDirectory(path,directoryPath);
+        }
+        else if(path.toString().endsWith(".json")){
+            Path directoryPath = Paths.get("C:/assignment/file_io/sorted/json");
+            createDirectory(path,directoryPath);
+        }
+        else if(path.toString().endsWith(".wma")){
+            Path directoryPath = Paths.get("C:/assignment/file_io/sorted/wma");
+            createDirectory(path,directoryPath);
+        }
+        else if(path.toString().endsWith(".gitignore")){
+            Path directoryPath = Paths.get("C:/assignment/file_io/sorted/gitignore");
+            createDirectory(path,directoryPath);
+        }
+        else if(path.toString().endsWith(".zip")){
+            Path directoryPath = Paths.get("C:/assignment/file_io/sorted/zip");
+            createDirectory(path,directoryPath);
+        }
+        else if(path.toString().endsWith(".sqlite3")){
+            Path directoryPath = Paths.get("C:/assignment/file_io/sorted/sqlite3");
+            createDirectory(path,directoryPath);
+        }
+        else if(path.toString().endsWith(".py")){
+            Path directoryPath = Paths.get("C:/assignment/file_io/sorted/py");
+            createDirectory(path,directoryPath);
+        }
 
-                Path newPath = directoryPath.resolve(path.getFileName());
-                Files.move(path, newPath, StandardCopyOption.REPLACE_EXISTING);
-                System.out.println("file is added to this folder");
 
-            } catch (IOException | DirectoryIteratorException ex) {
-                ex.printStackTrace();
-            }
+
+    }
+    private static void createDirectory(Path path, Path directoryPath){
+        try {
+            // if (Files.notExists(directoryPath)) {
+            Files.createDirectories(directoryPath);
+            // }
+
+            Path newPath = directoryPath.resolve(path.getFileName());
+            Files.move(path, newPath, StandardCopyOption.REPLACE_EXISTING);
+           // System.out.println("file is added to this folder");
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
 
     }
-}
+
+    }
+
